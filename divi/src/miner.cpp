0a67f2b069bc9333cffc2041cd6319410640e56f
Octal <octal@encke.com>
2020-05-09 12:17:54 -0600

    Dereference Pblock

--- divi/src/miner.cpp
+++ divi/src/miner.cpp
@@ -169,13 +169,12 @@ unsigned int GetBlockMinSize(unsigned int defaultBlockMinSize, unsigned int bloc
 
 class BlockMemoryPoolTransactionCollector
 {
-
 public:
     bool CollectTransactionsIntoBlock (
         unsigned int& nBlockMinSize, 
         unsigned int& nBlockPrioritySize, 
         unsigned int& nBlockMaxSize,
-        CBlock* pblock, 
+        CBlock& pblock, 
         unique_ptr<CBlockTemplate>&pblocktemplate,
         CAmount& nFees,
         bool& fProofOfStake,
@@ -340,7 +339,7 @@ public:
             UpdateCoins(tx, state, view, txundo, nHeight);
 
             // Added
-            pblock->vtx.push_back(tx);
+            pblock.vtx.push_back(tx);
             pblocktemplate->vTxFees.push_back(nTxFees);
             pblocktemplate->vTxSigOps.push_back(nTxSigOps);
             nBlockSize += nTxSize;
@@ -375,28 +374,28 @@ public:
         LogPrintf("CreateNewBlock(): total size %u\n", nBlockSize);
 
         // Compute final coinbase transaction.
-        pblock->vtx[0].vin[0].scriptSig = CScript() << nHeight << OP_0;
+        pblock.vtx[0].vin[0].scriptSig = CScript() << nHeight << OP_0;
         if (!fProofOfStake) {
             txNew.vout[0].nValue = GetBlockSubsidity(nHeight).nStakeReward;
             txNew.vin[0].scriptSig = CScript() << nHeight << OP_0;
-            pblock->vtx[0] = txNew;
+            pblock.vtx[0] = txNew;
             pblocktemplate->vTxFees[0] = -nFees;
         }
 
         // Fill in header
-        pblock->hashPrevBlock = pindexPrev->GetBlockHash();
+        pblock.hashPrevBlock = pindexPrev->GetBlockHash();
         if (!fProofOfStake)
-            UpdateTime(pblock, pindexPrev);
-        pblock->nBits = GetNextWorkRequired(pindexPrev, pblock);
-        pblock->nNonce = 0;
-        pblock->nAccumulatorCheckpoint = static_cast<uint256>(0);
-        pblocktemplate->vTxSigOps[0] = GetLegacySigOpCount(pblock->vtx[0]);
+            UpdateTime(&pblock, pindexPrev);
+        pblock.nBits = GetNextWorkRequired(pindexPrev, &pblock);
+        pblock.nNonce = 0;
+        pblock.nAccumulatorCheckpoint = static_cast<uint256>(0);
+        pblocktemplate->vTxSigOps[0] = GetLegacySigOpCount(pblock.vtx[0]);
 
         //byrd transaction printout
-        LogPrintf("CreateNewBlock(): block tostring %s\n", pblock->ToString());
+        LogPrintf("CreateNewBlock(): block tostring %s\n", pblock.ToString());
 
         CValidationState state;
-        if (!TestBlockValidity(state, *pblock, pindexPrev, false, false)) {
+        if (!TestBlockValidity(state, pblock, pindexPrev, false, false)) {
             LogPrintf("CreateNewBlock() : TestBlockValidity failed\n");
             mempool.clear();
             return false;
@@ -405,6 +404,7 @@ public:
         return true;
     }
 };
+
 CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, bool fProofOfStake)
 {
     CReserveKey reservekey(pwallet);
@@ -453,7 +453,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
             nBlockMinSize, 
             nBlockPrioritySize,
             nBlockMaxSize,
-            pblock,
+            *pblock,
             pblocktemplate,
             nFees, 
             fProofOfStake,
