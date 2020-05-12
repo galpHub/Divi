3fd802a4bf33a8ce10426b541b6430e246c53fd1
galp <the.talion@gmail.com>
2020-05-12 14:14:17 -0600

    Remove unused

--- divi/src/BIP9ActivationManager.cpp
+++ divi/src/BIP9ActivationManager.cpp
@@ -1,13 +1,5 @@
 #include <BIP9ActivationManager.h>
 
-#include <BIP9Deployment.h>
-
-static BIP9Deployment CurrentBIP9Deployments[1] =
-{
-    BIP9Deployment()
-};
-
-
 BIP9ActivationManager::BIP9ActivationManager(
     ): thresholdCaches_()
     , bip9ActivationTrackers_()
