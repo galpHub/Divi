132b4074f516a65aa989645136eec3cfbdfa57b8
kriskoin <cris7775@gmail.com>
2020-10-21 17:18:24 -0600

    add: soccketsController's source files added

---
+++ divi/src/sockControl.h
@@ -0,0 +1,9 @@
+
+// Copyright (c) 2017-2020 The DIVI Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef _SOCKCONTROL_H
+#define _SOCKCONTROL_H
+
+#endif
