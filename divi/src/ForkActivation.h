d5dd1ac02c85018f3ebe1b9f8f359236f545e7bc
Daniel Kraft <d@domob.eu>
2020-11-04 16:38:45 +0100

    Implement ActivationState class.
    This adds the new ActivationState utility class.  It is used to
    abstract fork activation away from other parts of the code.  An
    instance is based on a CBlockIndex representing the chain tip being
    validated, and it can be queried for the state of various forks
    with IsActive.
    
    Activation happens by block timestamp, as this is totally straight-forward
    and more or less the simplest form of activation possible.  This also
    makes it independent of the block context (unlike e.g. median time)
    and allows us to theoretically use fork activation even for things like
    changing the serialisation format of blocks.
    
    For now, no fork is defined apart from a dummy one used in testing.
    In the future, we will define real forks with this framework (e.g. the
    staking-vault changes).

---
+++ divi/src/ForkActivation.h
@@ -0,0 +1,58 @@
+// Copyright (c) 2020 The DIVI Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef FORK_ACTIVATION_H
+#define FORK_ACTIVATION_H
+
+class CBlockIndex;
+
+/**
+ * The list of consensus changes ("forks") that have been introduced
+ * on the network since its launch and may need to be activated
+ * (with conditional logic in other parts of the codebase depending
+ * on whether or not a fork is active).
+ */
+enum class Fork
+{
+
+  /* Test forks not actually deployed / active but used for unit tests.  */
+  TestByTimestamp,
+
+};
+
+/**
+ * Activation state of forks.  Each instance corresponds to a particular block,
+ * and can be used to query the state of each supported fork as it should be
+ * for validating this block.
+ */
+class ActivationState
+{
+
+private:
+
+  /**
+   * The chain tip this is associated for.  It is the block actually
+   * being validated using the rules from the current activation state.
+   */
+  const CBlockIndex* pindex;
+
+public:
+
+  explicit ActivationState(const CBlockIndex* pi);
+
+  ActivationState(ActivationState&&) = default;
+
+  ActivationState() = delete;
+  ActivationState(const ActivationState&) = delete;
+  void operator=(const ActivationState&) = delete;
+
+  /**
+   * Returns true if the indicated fork should be considered active
+   * for processing the associated block.
+   */
+  bool IsActive(Fork f) const;
+
+};
+
+#endif // FORK_ACTIVATION_H
---
+++ divi/src/test/ForkActivation_tests.cpp
@@ -0,0 +1,96 @@
+// Copyright (c) 2020 The DIVI Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "test_only.h"
+
+#include "ForkActivation.h"
+#include "chain.h"
+
+#include <cassert>
+#include <memory>
+#include <vector>
+
+namespace
+{
+
+/**
+ * Test fixture for the ActivationState.  It keeps track of a local
+ * dummy "chain" (just CBlockIndex instances), to which the tests can
+ * add new blocks with data as needed and then check the activation
+ * status against it.
+ */
+class TestContainer
+{
+
+private:
+
+  /**
+   * The CIndex entries we've built up for our test.  The last one
+   * will correspond to the "current tip".
+   */
+  std::vector<std::unique_ptr<CBlockIndex>> blocks;
+
+public:
+
+  TestContainer()
+  {
+    /* We always start with a genesis block of version 1, just as
+       in the real blockchain.  */
+    AddBlock()->nVersion = 1;
+  }
+
+  /**
+   * Adds a new block to the chain and returns it for setting
+   * data as needed.
+   */
+  CBlockIndex*
+  AddBlock()
+  {
+    CBlockIndex* pprev = nullptr;
+    if (!blocks.empty())
+      pprev = blocks.back().get();
+
+    blocks.emplace_back(new CBlockIndex());
+    blocks.back()->pprev = pprev;
+    blocks.back()->nVersion = 4;
+
+    return blocks.back().get();
+  }
+
+  /**
+   * Constructs an activation state instance based on the current chain.
+   */
+  ActivationState
+  CurrentState() const
+  {
+    assert(!blocks.empty());
+    return ActivationState(blocks.back().get());
+  }
+
+};
+
+} // anonymous namespace
+
+BOOST_FIXTURE_TEST_SUITE(ActivationState_tests, TestContainer)
+
+BOOST_AUTO_TEST_CASE(activationByTimestamp)
+{
+  /* Add some blocks with an early timestamp.  */
+  for (int i = 0; i < 20; ++i)
+    AddBlock()->nTime = 999999999;
+
+  /* One block beyond the timestamp is enough to activate the fork for it.  */
+  AddBlock()->nTime = 1000000000;
+  BOOST_CHECK_EQUAL(CurrentState().IsActive(Fork::TestByTimestamp), true);
+
+  /* The fork can temporarily get back to inactive as well (although that
+     won't be possible permanently in practice since the median time will
+     at some point enforce a block time beyond the activation).  */
+  for (int i = 0; i < 20; ++i)
+    AddBlock()->nTime = 1000000000;
+  AddBlock()->nTime = 999999999;
+  BOOST_CHECK_EQUAL(CurrentState().IsActive(Fork::TestByTimestamp), false);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
