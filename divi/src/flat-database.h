08c9f62cd408cb89fbcfed768b5d0e3ba7ea5759
Daniel Kraft <d@domob.eu>
2021-07-09 17:20:49 +0200

    Refactor CFlatDB.
    This refactors CFlatDB to move out the core logic of reading and
    writing checksummed chunks to a superclass.  We will use this in the
    future to implement a new append-only structure of multiple chunks.

--- divi/src/flat-database.h
+++ divi/src/flat-database.h
@@ -15,15 +15,19 @@
 
 #include <boost/filesystem.hpp>
 
-/** 
-*   Generic Dumping and Loading
-*   ---------------------------
-*/
-
-template<typename T>
-class CFlatDB
+/** Base class for file-based data storage.  It supports generic
+ *  reading and writing from/to files in checksumed chunks of data.  */
+class FlatDataFile
 {
+
 private:
+
+    const std::string strFilename;
+    const boost::filesystem::path pathDB;
+
+public:
+
+    class Reader;
 
     enum ReadResult {
         Ok,
@@ -35,26 +39,40 @@ private:
         IncorrectFormat
     };
 
-    boost::filesystem::path pathDB;
-    std::string strFilename;
-    std::string strMagicMessage;
+    explicit FlatDataFile(const std::string& strFilenameIn)
+      : strFilename(strFilenameIn), pathDB(GetDataDir() / strFilename)
+    {}
 
-    bool Write(const T& objToSave)
+    const std::string& Filename() const
+    {
+        return strFilename;
+    }
+
+    const boost::filesystem::path& Path() const
+    {
+        return pathDB;
+    }
+
+    /** Writes a single chunk of data to the file, using
+     *  the given magic bytes and optionally appending rather
+     *  than truncating the file.  */
+    template <typename T>
+        bool Write(const std::string& magic, const T& objToSave, bool append) const
     {
         // LOCK(objToSave.cs);
 
-        int64_t nStart = GetTimeMillis();
+        const int64_t nStart = GetTimeMillis();
 
         // serialize, checksum data up to that point, then append checksum
         CDataStream ssObj(SER_DISK, CLIENT_VERSION);
-        ssObj << strMagicMessage; // specific magic message for this type of object
+        ssObj << magic; // specific magic message for this type of object
         ssObj << FLATDATA(Params().MessageStart()); // network specific magic number
         ssObj << objToSave;
-        uint256 hash = Hash(ssObj.begin(), ssObj.end());
+        const uint256 hash = Hash(ssObj.begin(), ssObj.end());
         ssObj << hash;
 
         // open output file, and associate with CAutoFile
-        FILE *file = fopen(pathDB.string().c_str(), "wb");
+        FILE *file = fopen(pathDB.string().c_str(), append ? "ab" : "wb");
         CAutoFile fileout(file, SER_DISK, CLIENT_VERSION);
         if (fileout.IsNull())
             return error("%s: Failed to open file %s", __func__, pathDB.string());
@@ -63,7 +81,7 @@ private:
         try {
             fileout << ssObj;
         }
-        catch (std::exception &e) {
+        catch (const std::exception& e) {
             return error("%s: Serialize or I/O error - %s", __func__, e.what());
         }
         fileout.fclose();
@@ -74,86 +92,120 @@ private:
         return true;
     }
 
-    ReadResult Read(T& objToLoad, bool fDryRun = false)
+    /** Returns a reader instance for the underlying file.  */
+    std::unique_ptr<Reader> Read() const;
+
+};
+
+/** Helper class that corresponds to an opened data file and allows to
+ *  read it chunk-by-chunk.  This is a bit like an iterator.  */
+class FlatDataFile::Reader
+{
+
+private:
+
+    /** The underlying opened file.  */
+    CAutoFile file;
+
+    /** The magic string of the current chunk.  */
+    std::string magic;
+
+    /** If there is a current chunk of unparsed data, this holds
+     *  the stream to read.  */
+    std::unique_ptr<CDataStream> chunk;
+
+    explicit Reader(const boost::filesystem::path& pathDB)
+      : file(fopen(pathDB.string().c_str(), "rb"), SER_DISK, CLIENT_VERSION)
+    {}
+
+    friend class FlatDataFile;
+
+public:
+
+    CAutoFile& GetFile()
     {
-        //LOCK(objToLoad.cs);
+        return file;
+    }
 
-        int64_t nStart = GetTimeMillis();
-        // open input file, and associate with CAutoFile
-        FILE *file = fopen(pathDB.string().c_str(), "rb");
-        CAutoFile filein(file, SER_DISK, CLIENT_VERSION);
-        if (filein.IsNull())
-        {
-            error("%s: Failed to open file %s", __func__, pathDB.string());
-            return FileError;
-        }
+    /** Tries to read the next chunk of raw data from the file.  This parses
+     *  the magic, verifies the network version and checks the hash, but does
+     *  not yet attempt to parse the data into a particular instance.
+     *
+     *  The caller must pass in the expected size of the next chunk, which they
+     *  must obtain somehow themselves.  */
+    ReadResult NextChunk(size_t dataSize);
 
-        // use file size to size memory buffer
-        int fileSize = boost::filesystem::file_size(pathDB);
-        int dataSize = fileSize - sizeof(uint256);
-        // Don't try to resize to a negative number if file is small
-        if (dataSize < 0)
-            dataSize = 0;
-        std::vector<unsigned char> vchData;
-        vchData.resize(dataSize);
-        uint256 hashIn;
+    /** Returns the magic of the current chunk.  */
+    const std::string& GetMagic() const
+    {
+        assert(chunk != nullptr);
+        return magic;
+    }
 
-        // read data and checksum from file
+    /** Parses the current chunk into an object of type T.  */
+    template <typename T>
+        ReadResult ParseChunk(T& objToLoad)
+    {
+        assert(chunk != nullptr);
         try {
-            filein.read((char *)&vchData[0], dataSize);
-            filein >> hashIn;
-        }
-        catch (std::exception &e) {
-            error("%s: Deserialize or I/O error - %s", __func__, e.what());
-            return HashReadError;
-        }
-        filein.fclose();
-
-        CDataStream ssObj(vchData, SER_DISK, CLIENT_VERSION);
-
-        // verify stored checksum matches input data
-        uint256 hashTmp = Hash(ssObj.begin(), ssObj.end());
-        if (hashIn != hashTmp)
-        {
-            error("%s: Checksum mismatch, data corrupted", __func__);
-            return IncorrectHash;
-        }
-
-
-        unsigned char pchMsgTmp[4];
-        std::string strMagicMessageTmp;
-        try {
-            // de-serialize file header (file specific magic message) and ..
-            ssObj >> strMagicMessageTmp;
-
-            // ... verify the message matches predefined one
-            if (strMagicMessage != strMagicMessageTmp)
-            {
-                error("%s: Invalid magic message", __func__);
-                return IncorrectMagicMessage;
-            }
-
-
-            // de-serialize file header (network specific magic number) and ..
-            ssObj >> FLATDATA(pchMsgTmp);
-
-            // ... verify the network matches ours
-            if (memcmp(pchMsgTmp, Params().MessageStart(), sizeof(pchMsgTmp)))
-            {
-                error("%s: Invalid network magic number", __func__);
-                return IncorrectMagicNumber;
-            }
-
             // de-serialize data into T object
-            ssObj >> objToLoad;
-        }
-        catch (std::exception &e) {
+            *chunk >> objToLoad;
+            chunk.reset();
+        } catch (const std::exception& e) {
             objToLoad.Clear();
             error("%s: Deserialize or I/O error - %s", __func__, e.what());
             return IncorrectFormat;
         }
+        return Ok;
+    }
 
-        LogPrintf("Loaded info from %s  %dms\n", strFilename, GetTimeMillis() - nStart);
+};
+
+/** Flat data file that stores a single chunk and is thus able to dump
+ *  and load one instance of some class T.  */
+template<typename T>
+class CFlatDB : private FlatDataFile
+{
+
+private:
+
+    const std::string strMagicMessage;
+
+    bool Write(const T& objToSave) const
+    {
+        return FlatDataFile::Write(strMagicMessage, objToSave, false);
+    }
+
+    ReadResult Read(T& objToLoad, bool fDryRun = false) const
+    {
+        const int64_t nStart = GetTimeMillis();
+
+        auto reader = FlatDataFile::Read();
+        if (reader == nullptr)
+            return FileError;
+
+        // use file size to size memory buffer
+        const int fileSize = boost::filesystem::file_size(Path());
+        int dataSize = fileSize - sizeof(uint256);
+        // Don't try to resize to a negative number if file is small
+        if (dataSize < 0)
+            dataSize = 0;
+
+        ReadResult res = reader->NextChunk(dataSize);
+        if (res != Ok)
+            return res;
+
+        if (reader->GetMagic() != strMagicMessage)
+        {
+            error("%s: Invalid magic message", __func__);
+            return IncorrectMagicMessage;
+        }
+
+        res = reader->ParseChunk(objToLoad);
+        if (res != Ok)
+            return res;
+
+        LogPrintf("Loaded info from %s  %dms\n", Filename(), GetTimeMillis() - nStart);
         LogPrintf("     %s\n", objToLoad.ToString());
         if(!fDryRun) {
             LogPrintf("%s: Cleaning....\n", __func__);
@@ -164,24 +216,21 @@ private:
         return Ok;
     }
 
-
 public:
-    CFlatDB(std::string strFilenameIn, std::string strMagicMessageIn)
-    {
-        pathDB = GetDataDir() / strFilenameIn;
-        strFilename = strFilenameIn;
-        strMagicMessage = strMagicMessageIn;
-    }
 
-    bool Load(T& objToLoad)
+    explicit CFlatDB(const std::string& strFilename, const std::string& strMagicMessageIn)
+      : FlatDataFile(strFilename), strMagicMessage(strMagicMessageIn)
+    {}
+
+    bool Load(T& objToLoad) const
     {
-        LogPrintf("Reading info from %s...\n", strFilename);
+        LogPrintf("Reading info from %s...\n", Filename());
         ReadResult readResult = Read(objToLoad);
         if (readResult == FileError)
-            LogPrintf("Missing file %s, will try to recreate\n", strFilename);
+            LogPrintf("Missing file %s, will try to recreate\n", Filename());
         else if (readResult != Ok)
         {
-            LogPrintf("Error reading %s: ", strFilename);
+            LogPrintf("Error reading %s: ", Filename());
             if(readResult == IncorrectFormat)
             {
                 LogPrintf("%s: Magic is ok but data has invalid format, will try to recreate\n", __func__);
@@ -195,20 +244,20 @@ public:
         return true;
     }
 
-    bool Dump(T& objToSave)
+    bool Dump(T& objToSave) const
     {
         int64_t nStart = GetTimeMillis();
 
-        LogPrintf("Verifying %s format...\n", strFilename);
+        LogPrintf("Verifying %s format...\n", Filename());
         T tmpObjToLoad;
         ReadResult readResult = Read(tmpObjToLoad, true);
 
         // there was an error and it was not an error on file opening => do not proceed
         if (readResult == FileError)
-            LogPrintf("Missing file %s, will try to recreate\n", strFilename);
+            LogPrintf("Missing file %s, will try to recreate\n", Filename());
         else if (readResult != Ok)
         {
-            LogPrintf("Error reading %s: ", strFilename);
+            LogPrintf("Error reading %s: ", Filename());
             if(readResult == IncorrectFormat)
                 LogPrintf("%s: Magic is ok but data has invalid format, will try to recreate\n", __func__);
             else
@@ -218,9 +267,9 @@ public:
             }
         }
 
-        LogPrintf("Writing info to %s...\n", strFilename);
+        LogPrintf("Writing info to %s...\n", Filename());
         Write(objToSave);
-        LogPrintf("%s dump finished  %dms\n", strFilename, GetTimeMillis() - nStart);
+        LogPrintf("%s dump finished  %dms\n", Filename(), GetTimeMillis() - nStart);
 
         return true;
     }
